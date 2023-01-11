31446a3024b2ae114978d177e53d4cdfbd7d7472
Diogo Ferreira Saucedo <Diogoferreiradfs@hotmail.com>
2023-01-11 16:10:41 -0400

    feat(services): add database services

---
+++ src/services/TodoService.ts
@@ -0,0 +1,17 @@
+import { ITodo } from 'interfaces';
+import { Api } from 'providers';
+
+type TodoSend = Pick<ITodo, 'task' | 'isDone'>;
+
+const getAllTodos = () => Api.get<ITodo[]>('/v1/todos');
+
+const createTodo = (todo: TodoSend) => Api.post('/v1/todos', todo);
+
+const updateTodo = (id: string, todo: TodoSend) =>
+  Api.put(`/v1/todos/${id}`, todo);
+
+export const TodoService = {
+  getAllTodos,
+  createTodo,
+  updateTodo,
+};
